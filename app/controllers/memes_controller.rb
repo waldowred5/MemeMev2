f26fdaf1c54b1c1fe12651a8ed53d852ac299b97
waldowred5 <dwaldow@me.com>
2020-11-03 22:26:23 +1100

    Fixed meme show page

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,6 +1,6 @@
 class MemesController < ApplicationController
   before_action :set_meme, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token, :only => [:create]
+  # skip_before_action :verify_authenticity_token, :only => [:create]
 
   # GET /memes
   # GET /memes.json
@@ -70,6 +70,6 @@ class MemesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def meme_params
-      params.fetch(:meme, {})
+      params.require(:meme).permit(:name)
     end
 end
--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,12 +1,2 @@
 class Meme < ApplicationRecord
-    def create
-        @meme = Meme.create(meme_params)
-        redirect_to @meme
-    end
-
-    private 
-    
-    def meme_params
-        params.require(:meme).permit(:name)
-    end
 end
