94b8b706e71c32dd2b8149d5f9389460812a91b0
waldowred5 <dwaldow@me.com>
2020-11-03 11:29:01 +1100

    Created User with Devise, Created Devise and User views/controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :memes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root to: "meme#index"
 end
